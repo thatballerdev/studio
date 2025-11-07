85f26e1dd8604b79511c1da2f7d1329b1aadeea0
Onem Danazumi <onemdanazumi.dev@gmail.com>
2025-11-07 21:33:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7345835072-fa19b",
+  "appId": "1:137171377701:web:b28aa4b1b92cef85fba9cc",
+  "apiKey": "AIzaSyCBGJUC8au0t659e_dNQyGfTrBaqMX_OMQ",
+  "authDomain": "studio-7345835072-fa19b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "137171377701"
+};
