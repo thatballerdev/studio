baf0645733ee07a8f855621c219df03416b1f304
Onem Danazumi <onemdanazumi.dev@gmail.com>
2025-11-12 23:53:39 +0000

    am sure that this would work https://miro.medium.com/v2/resize:fit:720/f

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ const Logo = ({
 }) => {
   return (
     <Image
-      src="https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30808-6/582426489_122098670349117932_2698552543375789505_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEuyKIYxW7Dn3qxNqiCzWpe3AeYBFP32h_cB5gEU_faH5nUkc6I6c9jsw6VRekvIgxHxPAH8IA3lOLAgxi7DfDd&_nc_ohc=n7OM5e4Q4l8Q7kNvwFNdotE&_nc_oc=Adns7cww7c5GrMra0kquSCME5Ar_Y2LhMLx45aDVpH-GXQtb0Rwd0vD0eIT1HCODJf4&_nc_zt=23&_nc_ht=scontent.fabv2-2.fna&_nc_gid=6zEUzxWfi9duEpIfYoxObw&oh=00_AfiRgvJMSy7EBW7wC-JdLQppjx8OGGBMLBn0nvoD_qt21g&oe=691ADF4C"
+      src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*34Npvco8IZMZAPGRf3Q3rQ.png"
       alt="Northway Logo"
       width={width}
       height={height}
