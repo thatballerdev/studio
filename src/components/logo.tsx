9ec282952468ff09d5f2ee56f866414ad9e4523a
Onem Danazumi <onemdanazumi.dev@gmail.com>
2025-11-12 23:27:17 +0000

    i understand, here is it https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ const Logo = ({
 }) => {
   return (
     <Image
-      src="https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30808-6/582415192_122098663035117932_16288885525470881_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF-kk2fqJ2CpkfqxIR4rFIoRhhOX9ma8apGGE5f2ZrxqpB4Xmw_JVKh78LicTJpR5mA3MzjzmXdYpTeqwPgHfBr&_nc_ohc=-F2B40h1WRYQ7kNvwGbJtGm&_nc_oc=Admj8Gar7lMYfUPxSU2YNQBytERW_IYsqfStfVUiMP8K-OAKtQUV3ZQxriRJPqlVzgw&_nc_zt=23&_nc_ht=scontent.fabv2-2.fna&_nc_gid=8aa__VUxbBdGKwY5EM61PA&oh=00_Afh_0c_peo4I1EQeVU8Sc6-_Vjzu6QuPZiCFEZQKher2fA&oe=691AE97A"
+      src="https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30808-6/582426489_122098670349117932_2698552543375789505_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEuyKIYxW7Dn3qxNqiCzWpe3AeYBFP32h_cB5gEU_faH5nUkc6I6c9jsw6VRekvIgxHxPAH8IA3lOLAgxi7DfDd&_nc_ohc=n7OM5e4Q4l8Q7kNvwFNdotE&_nc_oc=Adns7cww7c5GrMra0kquSCME5Ar_Y2LhMLx45aDVpH-GXQtb0Rwd0vD0eIT1HCODJf4&_nc_zt=23&_nc_ht=scontent.fabv2-2.fna&_nc_gid=6zEUzxWfi9duEpIfYoxObw&oh=00_AfiRgvJMSy7EBW7wC-JdLQppjx8OGGBMLBn0nvoD_qt21g&oe=691ADF4C"
       alt="Northway Logo"
       width={width}
       height={height}
